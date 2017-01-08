784bb9912bf8b7ae4f2b63721794cbcd0d88e960
jamesming <jamesming@gmail.com>
2017-01-08 09:43:42 -0800

    update

--- src/crossdom.js
+++ src/crossdom.js
@@ -114,8 +114,12 @@ export default class CrossDomService {
 
 	launchPictographrFile( fileId: string){
 
-		// var url = 'https://pictographr.com/app?new_width=620&new_height=500&pollrefresh=true&state=%7B%22ids%22:%5B%22'
-		// 		+ fileId + '%22%5D,%22action%22:%22open%22,%22userId%22:%22' + this.google_id + '%22%7D';
+		var url = 'https://pictographr.com/app?new_width=620&new_height=500&pollrefresh=true&state=%7B%22ids%22:%5B%22'
+				+ fileId + '%22%5D,%22action%22:%22open%22,%22userId%22:%22' + this.google_id + '%22%7D';
+
+		window.location = url;
+
+		return;
 
 		console.log('Attempting to launch: ', fileId);
 

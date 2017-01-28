459a77df1582815d1a8901ea69c2ed03b7ef0786
jamesming <jamesming@gmail.com>
2017-01-28 08:59:34 -0800

    update

--- public/index.php
+++ public/index.php
@@ -46,6 +46,7 @@
     	console.log(test);
       var feed = (window.location.hostname.split('.')[0] === 'localhost' ?
           'teachingstash' : window.location.hostname.split('.')[0]);
+      if(feed === 'www') feed = window.location.hostname.split('.')[1])
       var host = 'https://pictographr.com/';
       var feedersite = host
           + 'feed/'

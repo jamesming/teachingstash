41607a59083725342ddca4a58c19f3514e23ac27
jamesming <jamesming@gmail.com>
2017-02-04 19:37:44 -0800

    update

--- public/php/sites.php
+++ public/php/sites.php
@@ -1,50 +1,42 @@
 <?php
-class Sites{
-	public function __construct() {
-		$this->__paramIntoProperties($_GET);
-		$this->siteroot = __DIR__. '/sites/' . $this->site;
-		$this->subdomainSegment = '/';
-		if( $_SERVER['HTTP_HOST'] != 'localhost' ) $this->siteroot = '/var/www/teachingstash/public/sites/' . $this->site;
-		if( isset($this->subdomain) ) {
-			$this->subdomainSegment = '/subdomains/' . $this->subdomain . '/';
+	class Sites{
+		public function __construct() {
+			$this->__paramIntoProperties($_GET);
+			$this->subdomainSegment = '/';
+			$this->siteroot = '/var/www/teachingstash/public/sites/' . $this->site;
+			if( isset($this->subdomain) ) {
+				$this->subdomainSegment = '/subdomains/' . $this->subdomain . '/';
+			}
+
+			$this->pathToJson = $this->siteroot . $this->subdomainSegment . 'data.json';
+			if($this->do == 'get') $this->getSiteJson();
+			if($this->do == 'set') $this->setSiteJson();
 		}
 
+		public function getSiteJson(){
+			$this->JSON = file_get_contents($this->pathToJson);
+			header('Access-Control-Allow-Origin: *');
+			echo $this->JSON ;
 
-		$url = 'http://staging.pictographr.com/sites/generateMenu?site=teachingstash&parentFolderId=0B1nKK3UKG5hjbk5Ba2dLNE9zUW8';
-		echo '<pre>'; print_r( json_decode($this->getSiteJson($url)) ); echo '</pre>';
-	}
+		}
 
-	public function getSiteJsonZ(){
-		$pathToJson = $this->siteroot . $this->subdomainSegment . 'site.json';
-		echo $pathToJson;
-		$this->JSON = file_get_contents($pathToJson);
-		header('Access-Control-Allow-Origin: *');
-		echo $this->JSON ;
+		public function getSiteJson(){
+			$data['title'] = $this->title;
+			$data['description'] = $this->discription;
+			$data['keywords'] = $this->keywords;
+			$this->JSON = file_put_contents($this->pathToJson, $data);
+			header('Access-Control-Allow-Origin: *');
+			echo json_encode($this->data);
+		}
 
-	}
-
-	public function getSiteJson($url){
-   	$ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    $data = curl_exec($ch);
-    curl_close($ch);
-
-    return $data;
+		private function __paramIntoProperties($input) {
+				foreach ($input as $k => $v) {
+					$this->{$k} = $v;
+				}
+				return $this;
+		}
 
 	}
 
-
-	private function __paramIntoProperties($input) {
-			foreach ($input as $k => $v) {
-				$this->{$k} = $v;
-			}
-			return $this;
-	}
-
-}
-new Sites();
+	new Sites();
 ?>

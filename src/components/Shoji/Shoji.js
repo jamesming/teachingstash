375e288b78bb8ad931ab84fe2366276c2a51b279
jamesming <jamesming@gmail.com>
2017-02-14 10:58:21 -0800

    update

--- src/components/Shoji/Shoji.js
+++ src/components/Shoji/Shoji.js
@@ -31,6 +31,7 @@ export default class ShojiCompnent extends React.Component {
     }
 
     slideFolderMenu() {
+      this.toast('hello');
       if (window.innerWidth < 768) {
         this.shoji.toggle('right', 130);
       }

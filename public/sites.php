475c44742c5d641d692824f962ef377f1ddf146f
jamesming <jamesming@gmail.com>
2017-02-23 15:16:26 -0800

    update

--- public/sites.php
+++ public/sites.php
@@ -11,6 +11,7 @@
 			$this->pathToJson = $this->siteroot . $this->subdomainSegment . 'data.json';
 			if($this->do == 'get') $this->getSiteJson();
 			if($this->do == 'set') $this->setSiteJson();
+			if($this->do == 'reset') $this->reset();
 		}
 
 		public function getSiteJson(){
@@ -30,6 +31,33 @@
 			$data['name'] = $this->organizationName;
 			$this->JSON = file_put_contents($this->pathToJson, json_encode($data));
 			$this->getSiteJson();
+		}
+
+		public function reset(){ // http://teachingstash.com/sites.php?do=reset&site=templatesforteachers
+
+			// derived from createMetaTagsForSubdomainsinSchools method in sites controller on pictographr
+
+			$str = file_get_contents('http://pictographr.com/sites/listdomains/');
+
+			$this->subdomainArray = json_decode($str, true);
+
+			$data = [];
+
+			foreach( $this->subdomainArray  as $idx => $record){
+
+				$data['name'] = $record['district'] . ' School District';
+				$data['title'] = $data['name'] . " Graphic Templates";
+				$data['description'] = 'Design Resources for Members of ' . $data['name'];
+				$data['keywords'] = 'One, Two, Three, Four';
+				$data['organization_id'] = '-1';
+				$data['partner_id'] = '-1';
+				$data['useDemo'] = '1';
+				$this->subdomainSegment = '/subomains/' . $record['name'] . '/';
+				$this->pathToJson = $this->siteroot . $this->subdomainSegment .  'data.json';
+				$data['pathToJson'] = $this->pathToJson;
+
+				file_put_contents($this->pathToJson, json_encode($data));
+			}
 		}
 
 		private function __paramIntoProperties($input) {

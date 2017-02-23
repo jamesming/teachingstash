5aca688a3cae85a33d3dd57f2d87dc2d51bbaa80
jamesming <jamesming@gmail.com>
2017-02-23 10:20:17 -0800

    update

--- src/components/Shoji/Nopermissiontoview.js
+++ src/components/Shoji/Nopermissiontoview.js
@@ -5,6 +5,17 @@ export default class Nopermissiontoview extends React.Component {
 
   render() {
 		const logoFileId = this.props.site.logoUrl.split('=')[1];
+    const resources = (window.location.hostname.split('.')[0] === 'localhost' ?
+            host
+          + 'sites/'
+          + site
+          + '/'
+    :
+            host
+          + 'sites/'
+          + site
+          + subdomainSegment
+    );
     return (
       <div className={classStyle.middleDiv}>
     		<div  className={classStyle.head}>
